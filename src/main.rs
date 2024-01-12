259173cdb61443286fbe25e300f0f30179f69dab
JethroNatividad <natividad.jet@gmail.com>
2024-01-12 23:12:16 +0800

    rename test fn

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,20 @@ mod tests {
 
     #[test]
     fn test_number_to_month() {
-        assert_eq!(test_number_to_month(0), "Invalid Month");
-        assert_eq!(test_number_to_month(1), "January");
-        assert_eq!(test_number_to_month(2), "February");
-        assert_eq!(test_number_to_month(3), "March");
-        assert_eq!(test_number_to_month(4), "April");
-        assert_eq!(test_number_to_month(5), "May");
-        assert_eq!(test_number_to_month(6), "June");
-        assert_eq!(test_number_to_month(7), "July");
-        assert_eq!(test_number_to_month(8), "August");
-        assert_eq!(test_number_to_month(9), "September");
-        assert_eq!(test_number_to_month(10), "October");
-        assert_eq!(test_number_to_month(11), "November");
-        assert_eq!(test_number_to_month(12), "December");
-        assert_eq!(test_number_to_month(99), "Invalid Month");
+        assert_eq!(number_to_month(0), "Invalid Month");
+        assert_eq!(number_to_month(1), "January");
+        assert_eq!(number_to_month(2), "February");
+        assert_eq!(number_to_month(3), "March");
+        assert_eq!(number_to_month(4), "April");
+        assert_eq!(number_to_month(5), "May");
+        assert_eq!(number_to_month(6), "June");
+        assert_eq!(number_to_month(7), "July");
+        assert_eq!(number_to_month(8), "August");
+        assert_eq!(number_to_month(9), "September");
+        assert_eq!(number_to_month(10), "October");
+        assert_eq!(number_to_month(11), "November");
+        assert_eq!(number_to_month(12), "December");
+        assert_eq!(number_to_month(99), "Invalid Month");
     }
 }
 
