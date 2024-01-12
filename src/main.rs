08afaff1fd6fabdf921c110d840a16d3964d36ee
JethroNatividad <natividad.jet@gmail.com>
2024-01-12 23:18:37 +0800

    implement number to month function, write more tests

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,29 @@
 // process: get corresponding month
 // output: month
 
-fn number_to_month(number: u32) -> &str {}
+fn number_to_month(number: u32) -> &str {
+    match number {
+        1 => "January",
+        2 => "February",
+        3 => "March",
+        4 => "April",
+        5 => "May",
+        6 => "June",
+        7 => "July",
+        8 => "August",
+        9 => "September",
+        10 => "October",
+        11 => "November",
+        12 => "December",
+        _ => panic!("Invalid month number. Must be between 1 and 12 inclusive."),
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_number_to_month() {
-        assert_eq!(number_to_month(0), "Invalid Month");
         assert_eq!(number_to_month(1), "January");
         assert_eq!(number_to_month(2), "February");
         assert_eq!(number_to_month(3), "March");
@@ -23,7 +38,13 @@ mod tests {
         assert_eq!(number_to_month(10), "October");
         assert_eq!(number_to_month(11), "November");
         assert_eq!(number_to_month(12), "December");
-        assert_eq!(number_to_month(99), "Invalid Month");
+
+        #[should_panic]
+        number_to_month(0);
+        #[should_panic]
+        number_to_month(13);
+        #[should_panic]
+        number_to_month(99);
     }
 }
 
