679be58eab1ea33aeaff69dbdf1925264ea1032b
JethroNatividad <natividad.jet@gmail.com>
2024-01-12 23:11:39 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@
 // process: get corresponding month
 // output: month
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_number_to_month() {
+        assert_eq!(test_number_to_month(1), "January");
+        assert_eq!(test_number_to_month(2), "February");
+        assert_eq!(test_number_to_month(3), "March");
+        assert_eq!(test_number_to_month(4), "April");
+        assert_eq!(test_number_to_month(5), "May");
+        assert_eq!(test_number_to_month(6), "June");
+        assert_eq!(test_number_to_month(7), "July");
+        assert_eq!(test_number_to_month(8), "August");
+        assert_eq!(test_number_to_month(9), "September");
+        assert_eq!(test_number_to_month(10), "October");
+        assert_eq!(test_number_to_month(11), "November");
+        assert_eq!(test_number_to_month(12), "December");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
